65ea0dd545fa98c752b8a70ecc9a990c5076ecac
Marshall Livingston <mltechi@pm.me>
2020-12-20 17:24:36 -0700

    Cleaning up 2020

--- movie_search/movie_search/search/views.py
+++ movie_search/movie_search/search/views.py
@@ -4,7 +4,8 @@ import json
 import argparse
 import os
 
-movies = []
+
+
 
 class Movie:
     def __init__(self):
@@ -18,30 +19,54 @@ class Movie:
     rating = 0
     image = ""
 
-name = "Trousers" ## Allow user to pass string (title) parameter
-
-r = requests.get(f'https://yts.mx/api/v2/list_movies.json?query_term={name}&sort_by=rating').json()
-i = 0
-for i in range(0, 20):
-    try:
-        movie = Movie()
-        movie.name = json.dumps(r['data']['movies'][i]['title_long'], indent=2)
-        movie.year = json.dumps(r['data']['movies'][i]['year'], indent=2)
-        movie.m_hash = json.dumps(r['data']['movies'][i]['torrents'][0]['hash'], indent=2)
-        mh = movie.m_hash.strip(' \" ')
-        movie.m_hash = f"https://yts.mx/torrent/download/{mh}"
-        movie.rating = json.dumps(r['data']['movies'][i]['rating'], indent=2)
-        movies.append(movie)
-    except IndexError:
-        pass
+## POC test
+## Ultimately allow user to search
 
 def home(request):
+
     if request.POST == '':
         pass
     else:
-        os.system(f"qbittorrent-nox {request.POST.get('movie_name')} --save-path=/home/tree/Documents/plex/movies/ --skip-dialog=true")
-    context = {
-        'movies': movies,
-        'title': 'Private Torrents',
-    }
-    return render(request, 'search/index.html', context)
+        if request.POST.get('movie') == None:
+            pass
+        else:
+            movies = []
+            movie = request.POST.get('movie')
+            r = requests.get(f'https://yts.mx/api/v2/list_movies.json?query_term={movie}&sort_by=rating').json()
+            i = 0
+            for i in range(0, 20):
+                try:
+                    movie = Movie()
+                    movie.name = json.dumps(r['data']['movies'][i]['title_long'], indent=2)
+                    movie.imdb = json.dumps(r['data']['movies'][i]['imdb_code'], indent=2).strip("\"")
+                    movie.summary = json.dumps(r['data']['movies'][i]['summary'], indent=2)
+                    movie.year = json.dumps(r['data']['movies'][i]['year'], indent=2)
+                    movie.m_hash = json.dumps(r['data']['movies'][i]['torrents'][0]['hash'], indent=2)
+                    mh = movie.m_hash.strip(' \" ')
+                    movie.m_hash = f"https://yts.mx/torrent/download/{mh}"
+                    movie.rating = json.dumps(r['data']['movies'][i]['rating'], indent=2)
+                    movies.append(movie)
+                except IndexError or KeyError:
+                    pass
+            context = {
+                'movies': movies,
+                'title': 'Private Torrents',
+            }
+            return render(request, 'search/index.html', context)
+
+            print(f"searching torrent: {request.POST.get('movie')}")
+            request.POST.get('movie')
+
+            # print(request.POST.get('movie'))
+        # os.system(f"qbittorrent-nox {request.POST.get('movie_name')} --save-path=os.environ('TOR') --skip-dialog=true")
+        # print(request.POST.get('movie_name'))
+        
+        if request.POST.get('movie_name') == None:
+            pass
+        else:
+            print(request.POST.get('movie_name'))
+            return (render(request, 'search/success.html'))
+    # context = {
+    #     'title': 'Private Torrents',
+    # }
+    return render(request, 'search/index.html', {'title' : 'Private Torrentz'} )
